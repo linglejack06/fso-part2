ae4a38cf18fa8b0c8284c98e08f0d5a2116cfd05
Jack Lingle <captainjack0818@gmail.com>
2023-06-13 17:00:21 -0400

    added fetched data into app.jsx

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter';
 import AddForm from './components/AddForm';
 import PersonList from './components/PersonList'
 
 function App() {
-  const [persons, setPersons] = useState([
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  useEffect(() => {
+    fetch('http://localhost:3000/persons')
+      .then((response) => response.json())
+      .then((data) => setPersons(data))
+  }, [])
   const personsToShow = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
   const handleNameChange = (e) => {
     setNewName(e.target.value);
