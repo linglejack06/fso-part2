0f90c0e6c32009b4dced8b48635b082e80fa30eb
Jack Lingle <captainjack0818@gmail.com>
2023-06-14 11:17:39 -0400

    add style and error component

--- lessons/src/App.js
+++ lessons/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import Note from './components/Note';
+import Notification from './components/Notification'
 import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('an error occured');
   useEffect(() => {
     noteService.getAll()
       .then((initialData) => setNotes(initialData));
@@ -35,13 +37,17 @@ const App = () => {
     noteService.update(id, changedNote)
       .then((data) => setNotes(notes.map(note => note.id !== id ? note : data)))
       .catch(() => {
-        alert(`The note ${note.content} was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => setErrorMessage(null), 5000);
         setNotes(notes.filter((note) => note.id !== id));
       })
   }
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <button onClick={() => setShowAll(!showAll)}>
         show {showAll ? 'important' : 'all'}
       </button>
